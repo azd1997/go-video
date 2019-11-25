9ae4f0fd77cf915c240fe6480e36601850d5db46
azd1997 <374192922@qq.com>
2019-11-25 00:27:26 -0500

    20191125-api-dbops-api-2

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -2,12 +2,46 @@ package dbops
 
 import (
 	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 func AddUserCredential(loginName string, pwd string) error {
 	// 可以直接使用包内dbConn变量
+	
+	stmtIns, err := dbConn.Prepare("INSERT INTO users (login_name, pwd)")		// prepare 预编译
+	if err != nil {
+		return err
+	}
+	stmtIns.Exec(loginName, pwd)
+	stmtIns.Close()
+	return nil
 }
 
 func GetUserCredential(loginName string) (string, error) {
 	// 可以直接使用包内dbConn变量
+
+	stmtOut, err := dbConn.Prepare("SELECT pwd FROM users WHERE login_name=")
+	if err != nil {
+		log.Printf("%s", err)
+		return "", err
+	}
+	
+	var pwd string
+	stmtOut.QueryRow(loginName).Scan(&pwd)
+	stmtOut.Close()
+	
+	return pwd, nil
+}
+
+func DeleteUser(loginName string, pwd string) error {
+	stmDel, err := dbConn.Prepare("DELETE FROM users WHERE login_name=")
+	if err != nil {
+		log.Printf("%s", err)
+		return err
+	}
+	
+	stmDel.Exec(loginName, pwd)
+	stmDel.Close()
+
+	return nil
 }
