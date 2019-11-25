12f5440924c94df494831c40ed28f66c6846c0c2
azd1997 <374192922@qq.com>
2019-11-25 04:02:05 -0500

    20191125-api-dbops-comments-test

--- api/dbops/api_comments_test.go
+++ api/dbops/api_comments_test.go
@@ -1,3 +1,38 @@
 package dbops
 
+import (
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestCommentsWorkFlow(t *testing.T) {
+	clearTables()
+	t.Run("PrepareUser", testAddUser)
+	t.Run("AddComment", testAddNewComment)
+	t.Run("ListComments", testListComments)
+}
+
+func testAddNewComment(t *testing.T) {
+	vid, aid, content := "123456", 1, "I like this video"
+	err := AddNewComment(vid, aid, content)
+	if err != nil {
+		t.Errorf("AddComment: %v\n", err)
+	}
+}
+
+func testListComments(t *testing.T) {
+	vid, from := "123456", 1514764800
+	to, _ := strconv.Atoi(strconv.FormatInt(time.Now().UnixNano()/1000000000, 10))
+
+	res, err := ListComments(vid, from, to)
+	if err != nil {
+		t.Errorf("ListComments: %v\n", err)
+	}
+
+	for i, elem := range res {
+		fmt.Printf("comment: %d, %v\n", i, elem)
+	}
+}
 
