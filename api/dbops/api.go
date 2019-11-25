21e029aeb9c83c4d2417485018b6b68f358803b8
azd1997 <374192922@qq.com>
2019-11-25 01:42:38 -0500

    20191125-api-dbops-api

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -1,19 +1,26 @@
 package dbops
 
 import (
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
 
 func AddUserCredential(loginName string, pwd string) error {
 	// 可以直接使用包内dbConn变量
-	
+
+	// stmt statement 是sql内的一个结构体，表示一个操作
 	stmtIns, err := dbConn.Prepare("INSERT INTO users (login_name, pwd) VALUES (?, ?)")		// prepare 预编译
 	if err != nil {
 		return err
 	}
+
 	stmtIns.Exec(loginName, pwd)
-	stmtIns.Close()
+	if err != nil {
+		return err
+	}
+	defer stmtIns.Close()
+
 	return nil
 }
 
@@ -28,20 +35,27 @@ func GetUserCredential(loginName string) (string, error) {
 	
 	var pwd string
 	stmtOut.QueryRow(loginName).Scan(&pwd)
-	stmtOut.Close()
+	if err != nil && err != sql.ErrNoRows {		// 当查询到没有该行时一样会返回一个row去扫描给pwd，这样的情况下也会返回给err，
+												// 但它其实是数据库没有该条记录我们应该额外在业务层面处理
+		return "", err
+	}
+	defer stmtOut.Close()
 	
 	return pwd, nil
 }
 
 func DeleteUserCredential(loginName string, pwd string) error {
-	stmDel, err := dbConn.Prepare("DELETE FROM users WHERE login_name = ? AND pwd = ?")
+	stmtDel, err := dbConn.Prepare("DELETE FROM users WHERE login_name = ? AND pwd = ?")
 	if err != nil {
 		log.Printf("%s", err)
 		return err
 	}
 	
-	stmDel.Exec(loginName, pwd)
-	stmDel.Close()
+	stmtDel.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
+	defer stmtDel.Close()
 
 	return nil
 }
