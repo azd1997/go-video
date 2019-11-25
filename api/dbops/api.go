7657baf8dd1761e29817a3dcc83910c22be71441
azd1997 <374192922@qq.com>
2019-11-25 00:02:14 -0500

    20191125-api-dbops-conn-1

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -1,24 +1,13 @@
 package dbops
 
 import (
-	"database/sql"		// 通用接口
-
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func openConn() *sql.DB {
-	dbConn, err := sql.Open("mysql", "root:123456!@#@tcp(localhost:3306)/go_video?charset=utf")		// charset这些应作为配置项处理，这里简化，直接写在这里面
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return dbConn
-}
-
 func AddUserCredential(loginName string, pwd string) error {
-	db := openConn()
+	// 可以直接使用包内dbConn变量
 }
 
 func GetUserCredential(loginName string) (string, error) {
-	db := openConn()
+	// 可以直接使用包内dbConn变量
 }
--- api/dbops/conn.go
+++ api/dbops/conn.go
@@ -1,3 +1,15 @@
 package dbops
 
+import "database/sql"
 
+var (
+	dbConn *sql.DB		// 包内全局变量，用于长连接
+	err error
+)
+
+func init() {
+	dbConn, err = sql.Open("mysql", "root:123456!@#@tcp(localhost:3306)/go_video?charset=utf")		// charset这些应作为配置项处理，这里简化，直接写在这里面
+	if err != nil {
+		panic(err.Error())
+	}
+}
