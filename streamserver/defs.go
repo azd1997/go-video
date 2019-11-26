04162d6ce6b4c2c9f235ca748289ec8177f56099
azd1997 <374192922@qq.com>
2019-11-25 20:44:00 -0500

    20191126-stream-upload

--- streamserver/defs.go
+++ streamserver/defs.go
@@ -3,4 +3,5 @@ package main
 
 const (
 	VIDEO_DIR = "./videos/"
+	MAX_UPLOAD_SIZE = 1024 * 1024 * 50 	// 单位是Byte, 设最大上传视频是50M
 )
--- streamserver/handlers.go
+++ streamserver/handlers.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/julienschmidt/httprouter"
+	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -13,6 +16,7 @@ func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) 
 
 	video, err := os.Open(videoFile)
 	if err != nil {
+		log.Printf("Open file error: %v\n", err)
 		sendErrorResponse(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		return
 	}
@@ -23,5 +27,34 @@ func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) 
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	r.Body = http.MaxBytesReader(w, r.Body, MAX_UPLOAD_SIZE)
+	if err := r.ParseMultipartForm(MAX_UPLOAD_SIZE); err != nil {
+		sendErrorResponse(w, http.StatusBadRequest, "File is too big")
+		return
+	}
 
+	file, _, err := r.FormFile("file")		// 获取客户端提交的表单，选中“file”(就是上传视频的键)
+	if err != nil {
+		sendErrorResponse(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Printf("Read file error: %v\n", err)
+		sendErrorResponse(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+
+	filename := p.ByName("vid-id")
+	err = ioutil.WriteFile(VIDEO_DIR+filename, data, 0666)
+	if err != nil {
+		log.Printf("Write file error: %v\n", err)
+		sendErrorResponse(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+
+	// 返回正确response
+	w.WriteHeader(http.StatusCreated)
+	io.WriteString(w, "Uploaded successfully")
 }
